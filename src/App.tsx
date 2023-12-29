3e219aafe87160e357a78a6a42baaa7364a6f427
Nguyen Khoi <minhkhoi1.04nhmk@gmail.com>
2023-12-29 21:43:01 +0700

    Refactor code to import App from "./App" and add new components for the HomePage layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
 import "./App.css";
-import { Carousel } from "./layouts/HomePage/Carousel";
-import { ExploreTopBooks } from "./layouts/HomePage/ExploreTopBooks";
-import { Heros } from "./layouts/HomePage/Heros";
-import { LibraryService } from "./layouts/HomePage/LibraryService";
+import { HomePage } from "./layouts/HomePage/HomePage";
 import { Footer } from "./layouts/NavbarAndFooter/Footer";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 
-function App() {
+export const App = () => {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryService />
+      <HomePage />
       <Footer />
     </div>
   );
-}
-
-export default App;
+};
