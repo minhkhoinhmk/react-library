7f3a30941ff9ea689b6f8244be7db43a9ee8efe2
Nguyen Khoi <minhkhoi1.04nhmk@gmail.com>
2024-01-01 15:43:00 +0700

    Add book checkout functionality and related components

---
+++ src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -0,0 +1,137 @@
+import { useEffect, useState } from "react";
+import BookModel from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import { StarsReview } from "../Utils/StarReview";
+import { CheckoutAndReviewBox } from "./CheckoutAndReviewBox";
+
+export const BookCheckoutPage = () => {
+  const [book, setBook] = useState<BookModel>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+
+  const bookId = window.location.pathname.split("/")[2];
+
+  useEffect(() => {
+    const fetchBook = async () => {
+      const baseUrl: string = `http://localhost:8080/api/books/${bookId}`;
+      const imageBaseUrl: string = "http://localhost:8080/api/v1";
+
+      const response = await fetch(baseUrl);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseJson = await response.json();
+
+      const loadedBook: BookModel = {
+        id: responseJson.id,
+        title: responseJson.title,
+        author: responseJson.author,
+        description: responseJson.description,
+        copies: responseJson.copies,
+        copiesAvailable: responseJson.copiesAvailable,
+        category: responseJson.category,
+        img: `${imageBaseUrl}?key=${responseJson.img}`,
+      };
+
+      if (loadedBook.img) {
+        const imageResponse = await fetch(loadedBook.img);
+
+        if (!imageResponse.ok) {
+          throw new Error("Something went wrong!");
+        }
+      }
+
+      setBook(loadedBook);
+      setIsLoading(false);
+    };
+    fetchBook().catch((error: any) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  console.log(isLoading);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <div className="container d-none d-lg-block">
+        <div className="row mt-5">
+          <div className="col-sm-4 col-md-4 d-flex justify-content-center align-items-center">
+            {isLoading && (
+              <div className="d-flex justify-content-center">
+                <div className="spinner-border text-primary" role="status">
+                  <span className="visually-hidden">Loading...</span>
+                </div>
+              </div>
+            )}
+            {book?.img ? (
+              <img src={book.img} width="226" height="349" alt="Book" />
+            ) : (
+              <img
+                src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+                width="226"
+                height="349"
+                alt="Book"
+              />
+            )}
+          </div>
+          <div className="col-4 col-md-4 container">
+            <div className="ml-2">
+              <h2>{book?.title}</h2>
+              <h5 className="text-primary">{book?.author}</h5>
+              <p className="lead">{book?.description}</p>
+              <StarsReview rating={4.6} size={32} />
+            </div>
+          </div>
+          <CheckoutAndReviewBox book={book} mobile={false} />
+        </div>
+        <hr />
+      </div>
+      <div className="container d-lg-none mt-5">
+        <div className="d-flex justify-content-center align-items-center">
+          {isLoading && (
+            <div className="d-flex justify-content-center">
+              <div className="spinner-border text-primary" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
+          )}
+          {book?.img ? (
+            <img src={book.img} width="226" height="349" alt="Book" />
+          ) : (
+            <img
+              src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
+              width="226"
+              height="349"
+              alt="Book"
+            />
+          )}
+        </div>
+        <div className="mt-4">
+          <div className="ml-2">
+            <h2>{book?.title}</h2>
+            <h5 className="text-primary">{book?.author}</h5>
+            <p className="lead">{book?.description}</p>
+            <StarsReview rating={4.6} size={32} />
+          </div>
+        </div>
+        <CheckoutAndReviewBox book={book} mobile={true} />
+        <hr />
+      </div>
+    </div>
+  );
+};
